4aa6e248480ace704ecb61abd53afd3a6bb8a104
Take-Take-gg <aperez65@ucol.mx>
2021-02-19 20:59:42 -0600

    Agregue la opcion 3 y 6

--- generador_figuras2D.py
+++ generador_figuras2D.py
@@ -1,6 +1,13 @@
 
+import math
+
+# figuras sera un listado de diccionarios
+figuras = []
+op = int
+contador = 0
 
 def main():
+    print("----------------------------")
     print("1.- Crear figura")
     print("2.- Listar una clasificación de figuras")
     print("3.- Listar todas las figuras")
@@ -8,65 +15,110 @@ def main():
     print("5.- Mostrar la suma de todos los perímetros")
     print("6.- Limpiar lista")
     print("0.- Salir")
-
-# figuras sera un listado de diccionarios
-figuras = []
-op = int
-contador = 0
-
+    print("----------------------------")
 
 def menu_figuras():
     global op
     while(op!=0):
         global contador
         contador += 1
+        print("----------------------------")
         print("Que figura desea anotar?")
         print("1.- Un cuadrado")
         print("2.- Un triangulo")
         print("3.- Un circulo")
+        print("9.- Para regresar al menu anterior")
         op=int(input("Escriba la opcion: "))
+        print("----------------------------")
         if op == 1:
             print("Ha seleccionado agregar un Cuadrado")
             lado = float(input("Dame el valor del lado del cuadrado: "))
+            print("----------------------------")
             crear_cuadrado(lado, contador)
         if op == 2:
             print("Ha seleccionado agregar un triangulo")
-            lado_a=float(input("Dame el valor del lado a del triangulo"))
-            lado_b=float(input("Dame el valor del lado b del triangulo"))
-            lado_c=float(input("Dame el valor del lado c del triangulo"))
+            print("Nota:si quieres un isoseles, que \"a\" y \"c\" sean iguales")
+            lado_a=float(input("Dame el valor del lado a del triangulo: "))
+            lado_b=float(input("Dame el valor del lado b del triangulo: "))
+            lado_c=float(input("Dame el valor del lado c del triangulo: "))
+            print("----------------------------")
             crear_triangulo(lado_a,lado_b,lado_c)
-
+        if op == 3:
+            print("Ha seleccionado agregar un circulo")
+            radio = float(input("Dame el valor del radio del circulo: "))
+            print("----------------------------")
+            crear_circulo(radio)
         else:
-            print("Escribe un numero del 1 al 3, o 0 para salir")
+            print("Escribe un numero del 1 al 3, o 0 para regresar al menu anterior")
+
+def crear_circulo(radio):
+    def area_circulo(radio):
+        res = math.pi * (radio ** 2)
+        return res
+    def perimetro_circulo(radio):
+        res = 2 * math.pi * radio
+        return res
+
+    area=area_circulo(radio)
+    perimetro = perimetro_circulo(radio)
+
+    diccionario_circulo = \
+        {
+            "id":contador,
+            "tipo":"Circulo",
+            "area":area,
+            "perimetro":perimetro
+        }
+    figuras.append(diccionario_circulo)
 
 def crear_triangulo(lado_a,lado_b,lado_c):
     
-    altura = float
-    
-    if(lado_a == lado_b and lado_c == lado_b):
-        print("se trata de un triangulo equilatero")
-        altura= ((3**1/3) * lado_a)/2
-
-    
-    if(lado_a==lado_c and lado_a !=lado_b and lado_c!=lado_b):
-        print("se trata de un triangulo isoseles")
-
-    if(lado_a != lado_b and lado_b!=lado_c and lado_a!=lado_c):
-        print("Se trata de un triangulo escaleno")
-
-
-
-
-    def area_triangulo(lado_a):
-        pass
+    def area_triangulo(lado,altura):
+        res = (lado*altura)/2
+        return res 
 
     def perimetro_triangulo(lado_a,lado_b,lado_c):
         res=lado_a+lado_b+lado_c
         return res
+
+    tipo=""
+    area=perimetro=altura=0.0
     
+    if(lado_a == lado_b and lado_c == lado_b):
+        print("se trata de un triangulo equilatero")
+        tipo="Triangulo Equilatero"
+        altura= ((3**(1/3)) * lado_a)/2
+        area=area_triangulo(lado_a,altura)
+        perimetro=perimetro_triangulo(lado_a,lado_b,lado_c)
 
 
+    
+    if(lado_a==lado_c and lado_a !=lado_b and lado_c!=lado_b):
+        print("se trata de un triangulo isoseles")
+        tipo="Triangulo Isoseles"
+        #Raiz de ((a^2 menos b^2))/4
+        altura = (((lado_a**2) - (lado_b**2))/4)**(1/2)
+        area = area_triangulo(lado_b,altura)
+        perimetro=perimetro_triangulo(lado_a,lado_b,lado_c)
 
+
+    if(lado_a != lado_b and lado_b!=lado_c and lado_a!=lado_c):
+        print("Se trata de un triangulo escaleno")
+        tipo="Triangulo Escaleno"
+        sp=(lado_a+lado_b+lado_c)/2
+        #raiz cuadrada de (sp x ( sp-lado_a)x(sp-lado_b) x (sp - lado_c))
+        area = (sp*(sp-lado_a)*(sp-lado_b)*(sp-lado_a))**(1/2)
+        perimetro=perimetro_triangulo(lado_a,lado_b,lado_c)
+
+
+    diccionario_triangulo = \
+        {
+            "id":contador,
+            "tipo":tipo,
+            "area":area,
+            "perimetro":perimetro
+        }
+    figuras.append(diccionario_triangulo)
 
 def crear_cuadrado(lado,contador):
     def area_cuadrado(lado):
@@ -82,22 +134,62 @@ def crear_cuadrado(lado,contador):
     peri_cuadradosss = peri_cuaddrado(lado)
     diccionario_cuadrado = \
         {
-            "numFig":contador,
-            "tipo":"Cuadrdado",
+            "id":contador,
+            "tipo":"Cuadrado",
             "area":area_cuadradosss,
             "perimetro":peri_cuadradosss
         }
     figuras.append(diccionario_cuadrado)
+
+#############################
+
+def listar_clasificaciones(clasificacion):
     
+    #dicc toma el valor del diccionario
+    for dicc in figuras:
+        for k,v in dicc.items():
+            print(f"Tenemos a {k} junto con {v}")
 
+            print(dicc['tipo':clasificacion])
 
+            if k == clasificacion:
+                print(f"UUUUUUUAaaaa{k} junto con {v}")
+                for k,v in dicc():
+                    print(f"Tenemos a {k} junto con {v}")
+                """
+                for k,v in dicc,items():
+                    print(f"Para cada {k} hay una {v}")"""
+                #its working
+                # print(f"Para cada {k} hay una {v}")
+    
 
 while(op!=0):
     main()
     op=int(input("Escriba una opcion: "))
     
+    if op==0:
+        print("See you later")
+
     if op == 1:
         menu_figuras()
+    if op == 2:
+        print("----------------------------")
+        print("Escriba el nombre de la figura que desea buscar")
+        print("Notas: solo responderan a los siguientes nombres: ")
+        print("Cuadrado, Triangulo Equilatero, Triangulo Isoseles,Triangulo Escaleno y Circulo")
+        clasificacion = input("Ahora si escriba: ")
+        listar_clasificaciones(clasificacion)
+    if op == 3:
+        print("----------------------------")
+        print(figuras)
+
+    #####
+    if op == 6:
+        print("Ha decidido borrar la lista")
+        figuras = []
+        print("Lista vaciada con exito")
+    
+    
 
     
 
